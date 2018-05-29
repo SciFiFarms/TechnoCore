04bd8a77b6e637b39a4758e4e49ffb0b559c6c1c
Spencer Hachmeister <SpencerHachmeister@gmail.com>
2018-05-29 17:54:29 -0600

    Changed from Dokuwiki to Gollum.

--- wiki/plugins/authldap/lang/sk/lang.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- *
- * @author Martin Michalek <michalek.dev@gmail.com>
- */
-$lang['connectfail']           = 'LDAP sa nemôže pripojiť: %s';
-$lang['domainfail']            = 'LDAP nemôže nájsť vaše meno (user dn)';
--- wiki/plugins/styling/lang/eo/lang.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- *
- * @author Kristjan SCHMIDT <kristjan.schmidt@googlemail.com>
- */
-$lang['btn_preview']           = 'Antaŭaj ŝanĝoj';
-$lang['btn_save']              = 'Konservi ŝanĝojn';
