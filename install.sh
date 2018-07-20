f20c63f8bb1d21ab3740fdaa5340631f44d7a969
Spencer Hachmeister <SpencerHachmeister@gmail.com>
2018-07-20 17:53:53 -0600

    These were the remaining changes nessesary to get All Thing running.

--- install.sh
+++ install.sh
@@ -15,21 +15,8 @@ mqtt=mqtt
 ha_db=ha_db
 nr=nr
 platformio=platformio
-wiki=wiki
-declare -a services=($vault $ha $mqtt $ha_db $nr $wiki $platformio)
-
-# Todo: only do this if not inited already.
-# I had to use  --advertise-addr 192.168.1.106. I imagine the IP address would change. 
-# I also had to install docker-compose seperaately. 
-#docker swarm init
-#docker swarm join localhost
-# had to add user to docker group: usermod -a -G docker spencer 
-
-# TODO: Should check that the stack is up before bringing it down.
-if [ $reinstall -eq 1 ] ; then
-    docker stack rm $stackname
-    sleep 10
-fi
+docs=docs
+declare -a services=($vault $ha $mqtt $ha_db $nr $docs $platformio)
 
 # Todo: only do this if not inited already.
 # I had to use  --advertise-addr 192.168.1.106. I imagine the IP address would change. 
@@ -56,8 +43,6 @@ export_UID_to_env
 docker-compose build
 network_name="${stackname}"
 docker network create --attachable $network_name
-
-docker network create $stackname
 
 if volume_exists vault && [ $reinstall -ne 1 ] ; then
     echo "Vault Initialized";
@@ -86,23 +71,6 @@ if volume_exists ha_db && [ $reinstall -ne 1 ] ; then
 else
     create_volume ha_db
     initialize_ha_db
-    add_CA_to_firefox
-fi
-
-if volume_exists mqtt && [ $reinstall -ne 1 ] ; then
-    echo "MQTT Initialized";
-else
-    create_volume mqtt
-    initialize_mqtt
-    create_vault_and_mqtt_user home_assistant
-    create_vault_and_mqtt_user node_red
-fi
-
-if volume_exists ha_db && [ $reinstall -ne 1 ] ; then
-    echo "Home Assistant database Initialized";
-else
-    create_volume ha_db
-    initialize_ha_db
 fi
 
 create_TLS_certs
@@ -112,12 +80,20 @@ docker network rm $network_name
 
 docker stack deploy --compose-file docker-compose.yml $stackname
 
-sleep 15
+# TODO: Make this check every # of seconds until it works. 
+sleep 30
 docker exec -it $(docker service ps -f desired-state=running --no-trunc althing_dev_vault | grep althing_dev | tr -s " " | cut -d " " -f 2).$(docker service ps -f desired-state=running --no-trunc althing_dev_vault | grep althing_dev | tr -s " " | cut -d " " -f 1) vault login $rootToken
+
+# TODO: This should actually verify the connection. It seems like the CA cert might not be getting passed into rabbithole. It's also possible that the MQTT service wasn't up yet.
+# This page had the code that showed what was going on for varification... Just creating a connection: https://github.com/hashicorp/vault/blob/e2bb2ec3b93a242a167f763684f93df867bb253d/builtin/logical/rabbitmq/path_config_connection.go
+# https://github.com/michaelklishin/rabbit-hole
+# https://github.com/hashicorp/vault/blob/e2bb2ec3b93a242a167f763684f93df867bb253d/builtin/logical/rabbitmq/secret_creds.go
+# https://github.com/hashicorp/vault/blob/e2bb2ec3b93a242a167f763684f93df867bb253d/builtin/logical/rabbitmq/path_role_create.go
 docker exec -it $(docker service ps -f desired-state=running --no-trunc althing_dev_vault | grep althing_dev | tr -s " " | cut -d " " -f 2).$(docker service ps -f desired-state=running --no-trunc althing_dev_vault | grep althing_dev | tr -s " " | cut -d " " -f 1) vault write rabbitmq/config/connection \
     connection_uri="https://mqtt:15672" \
     username="vault" \
-    password="$mqtt_password" 
+    password="$mqtt_password" \
+    verify_connection="false" # Description: If set, connection_uri is verified by actually connecting to the RabbitMQ management API. This simply allows us to set the connection without the server having to be up. 
 
 # Maybe pull a backup of the CA from docker secrets. Put in /etc/tls/althing.
 # Remove vault port
