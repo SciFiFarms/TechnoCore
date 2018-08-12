4f84b457406a4885a46c5c3d8f22788810f318bd
Spencer Hachmeister <SpencerHachmeister@gmail.com>
2018-08-11 18:13:58 -0600

    Add a script to build all the images.

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+tag=local
+docker build ../docs/ -t allthing/docs:$tag
+docker build ../home-assistant/ -t allthing/home-assistant:$tag
+docker build ../home-assistant-db/ -t allthing/home-assistant-db:$tag
+docker build ../node-red/ -t allthing/node-red:$tag
+docker build ../platformio/ -t allthing/platformio:$tag
+docker build ../rabbitmq/ -t allthing/rabbitmq:$tag
+docker build ../vault/ -t allthing/vault:$tag
