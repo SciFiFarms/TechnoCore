db9f49e2bee0c3643fb8ab653badb32729dcce3f
Spencer Hachmeister <SpencerHachmeister@gmail.com>
2018-09-21 11:04:01 -0700

    Fix remove_volume checks.

--- clean.sh
+++ clean.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 remove_volume() {
-    bad_text="No such volume"
-    response=$(docker volume rm $1)
-    while [ $? != 0 ]; do
-        grep -q 'No such volume' <<< $response 
-        if [ $? ]; then
+    local response # Declare response as a local var.
+    until response=$(docker volume rm $1 2>&1)
+    do 
+        #echo "$response"
+        if grep -q 'No such volume' <<< $response; then
             echo "Volume $1 doesn't exist, so it won't be removed."
             break
-        else
-            echo "Couldn't remove volume $1. Sleeping."
-            sleep 1
-            response=$(docker volume rm $1)
         fi
+        if grep -q 'volume is in use' <<< $response; then
+            local container=$(echo "$response" | cut -d"[" -f2 | cut -d"]" -f1 )
+            echo "Volume is in use by container $container. Will stop."
+            docker rm -f $container
+        fi
+        sleep 2
     done
     echo "Removed volume $1."
 }
