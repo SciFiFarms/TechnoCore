9c43edcc4e634f1d7d2d3f7f4956cc05527cba06
Spencer Hachmeister <SpencerHachmeister@gmail.com>
2018-09-21 11:04:52 -0700

    To clean.sh, add removal of vault volume and remove mqtt2 reference.

--- clean.sh
+++ clean.sh
@@ -24,4 +24,5 @@ remove_volume althing_dev_home-assistant-db-migrations
 remove_volume althing_dev_node-red
 remove_volume althing_dev_portainer
 remove_volume althing_dev_mqtt
-remove_volume althing_dev_mqtt2
+remove_volume althing_dev_vault
+#remove_volume althing_dev_mqtt2
