00f0ccb71fdc667025dea92b7f4f87336d219cae
Spencer Hachmeister <SpencerHachmeister@gmail.com>
2018-10-30 20:34:40 -0600

    Add image_provider .env var.

--- utilities/build.sh
+++ utilities/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-tag=local
-docker build ../docs/ -t allthing/docs:$tag
-docker build ../home-assistant/ -t allthing/home-assistant:$tag
-docker build ../home-assistant-db/ -t allthing/home-assistant-db:$tag
-docker build ../node-red/ -t allthing/node-red:$tag
-docker build ../platformio/ -t allthing/platformio:$tag
-docker build ../rabbitmq/ -t allthing/rabbitmq:$tag
-docker build ../vault/ -t allthing/vault:$tag
-docker build ../vernemq/ -t allthing/vernemq:$tag
-docker build ../portainer/ -t allthing/portainer:$tag
+technocore_folder=$(cd $(dirname $0) && pwd/..)
+source ${technocore_folder}/.env 
+docker build ./docs/ -t ${image_provider}/docs:$tag
+docker build ./home-assistant/ -t ${image_provider}/home-assistant:$tag
+docker build ./home-assistant-db/ -t ${image_provider}/home-assistant-db:$tag
+docker build ./node-red/ -t ${image_provider}/node-red:$tag
+docker build ./platformio/ -t ${image_provider}/platformio:$tag
+docker build ./vault/ -t ${image_provider}/vault:$tag
+docker build ./vernemq/ -t ${image_provider}/vernemq:$tag
+docker build ./portainer/ -t ${image_provider}/portainer:$tag
