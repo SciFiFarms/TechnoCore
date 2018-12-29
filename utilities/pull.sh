e1926f0d451acd062d7a3a44dafc06411df598b8
Spencer Hachmeister <SpencerHachmeister@gmail.com>
2018-12-29 14:04:56 -0700

    Add docker pull script to pull new versions of all images.

---
+++ utilities/pull.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+technocore_folder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${technocore_folder}/../.env 
+
+docker pull ${image_provider}/technocore-docs:$TAG
+docker pull ${image_provider}/technocore-home-assistant:$TAG
+docker pull ${image_provider}/technocore-home-assistant-db:$TAG
+docker pull ${image_provider}/technocore-node-red:$TAG
+docker pull ${image_provider}/technocore-platformio:$TAG
+docker pull ${image_provider}/technocore-platformio-wrapper:$TAG
+docker pull ${image_provider}/technocore-vault:$TAG
+docker pull ${image_provider}/technocore-vernemq:$TAG
+docker pull ${image_provider}/technocore-portainer:$TAG
+docker pull ${image_provider}/technocore-nginx:$TAG
