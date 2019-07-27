8c8d62a932b13163f00d82a66a2cb894b0dd1ff8
Spencer Hachmeister <SpencerHachmeister@gmail.com>
2019-07-27 16:01:52 -0600

    Fix variable checks in set_service_flag and default_to

--- lib/compose.sh
+++ lib/compose.sh
@@ -3,7 +3,8 @@
 # $1: The setting name
 # $2: The value to be set
 default_to (){
-    if [ ! "${!1}" ]; then
+    # This checks that the variable's existence. It WILL detect empty strings as set.
+    if [[ ! ${!1} && ${!1-unset} ]]; then
         export $1=$2
     fi
 }
@@ -61,16 +62,15 @@ secret_exists(){
 # $2: The default setting
 set_service_flag (){
     # This one is actually technocore.
-    local service_name=$1
+    local service_name=${1^^}
     local service_folder=${2:-$1}
     local service_flag=SERVICE_${service_name/-/_}
 
     # TODO: Could do some logic to detect if $2 suggests a no response. 
     default_to $service_flag $2
 
-    if [[ "${!service_flag}" ]]; then
-        config_location=$TECHNOCORE_SERVICES/${service_name}/compose.yml
-        export $service_flag=$config_location
+    if [[ "${!service_flag}" != "" ]]; then
+        config_location=$TECHNOCORE_SERVICES/${service_name,,}/compose.yml
         export SERVICE_CONFIG_${service_name/-/_}=$config_location
     fi
 }
@@ -92,7 +92,6 @@ get_compose(){
             echo "No services to load."
         fi
     done
-
 
     # Looping like this is prone to issues with whitespaces. I think this case is OK 
     # because the keys have to be single words and I'm not interested in the values. 
