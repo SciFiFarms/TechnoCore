e1baf235639f41f60e6ec960ed4b4e44c8a988b6
Spencer Hachmeister <SpencerHachmeister@gmail.com>
2019-11-02 17:00:31 -0600

    Add defaults.env file to set initial default values.

--- bin/init.sh
+++ bin/init.sh
@@ -7,6 +7,7 @@ set -a
     # TODO: I have this nagging feeling that doing it like this will make .env wipe out existing env vars.  
     #       https://gist.github.com/judy2k/7656bfe3b322d669ef75364a46327836
     if [ -f ".env" ]; then
+        . $TECHNOCORE_LIB/defaults.env
         . .env
     fi
 set +a
--- init.sh
+++ init.sh
@@ -10,5 +10,6 @@ set -a
 
     # TODO: I have this nagging feeling that doing it like this will make .env wipe out existing env vars.  
     #       https://gist.github.com/judy2k/7656bfe3b322d669ef75364a46327836
+    . $TECHNOCORE_LIB/defaults.env
     . ./.env
 set +a
