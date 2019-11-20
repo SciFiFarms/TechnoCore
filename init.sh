98c80ea1623f1798380626f8d86575bf0b92a23e
bengroneman <bluegroneman10@gmail.com>
2019-11-20 20:12:51 +0000

    fix: install docker pragmatically

---
+++ init.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+which docker
+if [ $? -eq 0 ]
+then
+	docker --version | grep "Docker version"
+	if [ $? -eq 0 ]
+        then echo "docker existing"
+	else
+        . ./docker-check.sh
+	fi
+	else
+        . ./docker-check.sh
+fi
+
+set -a
+    TECHNOCORE_ROOT=$(pwd)
+    TECHNOCORE_LIB=$TECHNOCORE_ROOT/lib
+    TECHNOCORE_SERVICES=$TECHNOCORE_ROOT/services
+
+    # TODO: add to path instead?
+    #TECHNOCORE_BIN=$TECHNOCORE_ROOT/bin
+
+    # TODO: I have this nagging feeling that doing it like this will make .env wipe out existing env vars.  
+    #       https://gist.github.com/judy2k/7656bfe3b322d669ef75364a46327836
+    . $TECHNOCORE_LIB/defaults.env
+    . ./.env
+set +a
